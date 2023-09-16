99fd9ef709c15ceaafa517f448733596bf8d1b04
ali-khan77 <143534651+ali-khan77@users.noreply.github.com>
2023-09-16 17:23:21 +0000

    test

--- sigma_plugins/_pm2.js
+++ sigma_plugins/_pm2.js
@@ -1,5 +1,5 @@
 
-const {Module_Export, Module_Exportss,tlang} = require('../lib')
+const {Module_Exports, Module_Exportss,tlang} = require('../lib')
 Module_Exports({   kingcmd: "restart", infocmd: "To restart bot",kingclass: "tools", kingpath: __filename }, async(Void, citel,text,{ isCreator }) => {  if (!isCreator) return citel.reply(tlang().owner);  const { exec } = require("child_process"); citel.reply('Restarting'); exec('pm2 restart all'); });
 
  
