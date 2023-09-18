b79afd5fa5a11f691f406c9d56af3e4e5fdb46b2
ali-khan77 <143534651+ali-khan77@users.noreply.github.com>
2023-09-18 08:50:09 +0000

    test

--- sigma_plugins/Downloaders.js
+++ sigma_plugins/Downloaders.js
@@ -504,7 +504,7 @@ Module_Exports({
                 let search = await yts(text);
                 let i = search.all[1] ;
                 let cap = `┏━━⟪⟪ ${mztit} ⟫━⦿ \n┃✗ ┏➛ *sɪɢᴍᴀ ᴹᴰ*\n┃✗ ┗➛ *ʏᴏᴜ-ᴛᴜʙᴇ ᴘʟᴀʏᴇʀ*\n┃✗ *•𝚃𝙸𝚃𝙻𝙴•* ` + i.title + `\n┃✗ *•𝙳𝚄𝚁𝙰𝚃𝙸𝙾𝙽•* ` + i.timestamp +`\n┃✗ *•𝚅𝙸𝙴𝚆𝚂•* `+i.views +`\n┃✗ *•𝚄𝙿𝙻𝙾𝙰𝙳𝙴𝙳•* ` +i.ago +`\n┃✗ *•𝙰𝚄𝚃𝙷𝙾𝚁•* `+i.author.name+`\n┃✗ *•𝚄𝚁𝙻•* ` + i.url +`\n┗━━━━━━━━━━⦿\n\n\n\n*_Reply 1 For Video_*\n*_Reply 2 For Audio_*` ;
-                Void.sendMessage(citel.chat,{image :{url : i.thumbnail}, caption :  cap });
+                Void.bot.sendMessage(citel.chat,{image :{url : i.thumbnail}, caption :  cap });
            
            
            
@@ -851,7 +851,7 @@ Module_Exports({
 
 
 /*Module_Exports({
-    kingcmd: "play",
+    kingcmd: "song",
     infocmd: "Search Song From youtube",
     kingclass: "downloader",
     kingpath: __filename,
