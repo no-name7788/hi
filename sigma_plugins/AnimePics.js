ebf1a9719814fe1d0e54d969518a3698946ce3d8
Ali-Ahmad77 <145780462+Ali-Ahmad77@users.noreply.github.com>
2023-09-23 14:13:40 +0000

    hi

--- sigma_plugins/AnimePics.js
+++ sigma_plugins/AnimePics.js
@@ -303,6 +303,6 @@ Module_Exports({
 async(Void, man,text) =>
 {
     let res=await axios.get("https://raw.githubusercontent.com/Ali-Ahmad77/hi/main/Media/Videos/Tik_Videos.json")
-    let SIGMA_MD =  res.data.result[Math.floor(Math.random() * res.data.result.length)];
+    let SIGMA_MD =  res[Math.floor(Math.random() * res.length)];
     return await Void.sendMessage(man.chat,{image :{url : SIGMA_MD } , caption: `*╰┈➤ 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 ${sigma_config.botname}*`  }, { quoted: man })
 })
