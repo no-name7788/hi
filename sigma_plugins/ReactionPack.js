fb2691f6c41f7db3c4c4a4b8eedb89ecd3740e2f
Ali-Ahmad77 <145780462+Ali-Ahmad77@users.noreply.github.com>
2023-09-23 09:27:58 +0000

    hi

--- sigma_plugins/ReactionPack.js
+++ sigma_plugins/ReactionPack.js
@@ -1,12 +1,5 @@
 /**
- Copyright (C) 2022.
- Licensed under the  GPL-3.0 License;
- You may not use this file except in compliance with the License.
- It is supplied in the hope that it may be useful.
- * @project_name : Secktor-Md
- * @author : SamPandey001 <https://github.com/SamPandey001>
- * @description : Secktor,A Multi-functional whatsapp bot.
- * @version 0.0.6
+
  
  Module_Exports({
             kingcmd: "reaction-pack",
@@ -14,6 +7,9 @@
             use: '<quote|reply|tag>',
         },
  **/
+/* These Commands Are Still Under Development
+
+
 
         const axios = require('axios')
         const { fetchJson,Module_Exports, GIFBufferToVideoBuffer} = require('../lib')
@@ -372,4 +368,4 @@
                     Void.sendMessage(citel.chat, { video: gif, gifPlayback: true, mentions: [citel.sender], caption: cap }, { quoted: citel });
                 }
             }
-        )
+        )*/
