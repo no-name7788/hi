7beac669a7a5896c2a503b788aed49fbeb21a675
Ali-Ahmad77 <145780462+Ali-Ahmad77@users.noreply.github.com>
2023-09-23 11:16:34 +0000

    hi

--- sigma_plugins/AnimePics.js
+++ sigma_plugins/AnimePics.js
@@ -276,6 +276,6 @@ Module_Exports({
 async(Void, man,text) =>
 {
     let res=await axios.get("https://raw.githubusercontent.com/Ali-Ahmad77/hi/main/Media/WallPapers/husbu.json")
-    let url =  res.data.result[Math.floor(Math.random() * res.data.result.length)];
+    let random = anu[Math.floor(Math.random() * anu.length)]
     return await Void.sendMessage(man.chat,{image :{url : random } , caption: `*╰┈➤ 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 ${sigma_config.botname}*`  }, { quoted: man })
 })
