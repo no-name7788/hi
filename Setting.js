97f34b9a7fb571b93957fb9c375b47da50062934
Ali-Ahmad77 <145780462+Ali-Ahmad77@users.noreply.github.com>
2023-09-25 16:18:18 +0500

    Update Setting.js

--- Setting.js
+++ Setting.js
@@ -10,36 +10,36 @@ global.audio = '' ;
 global.video = '' ;
 global.blockJids = process.env.BLOCK_JID ||'120363023983262391@g.us' ;
 global.allowJids = process.env.ALLOW_JID ||'120363022922797633@g.us' ;
-global.email = 'samsamsun789@gmail.com' ;
-global.github = 'https://github.com/SuhailTechInfo/Secktor-Md' ;
+global.email = 'HELP_SIGMA-MD@outlook.com' ;
+global.github = 'https://github.com/Maher-Zubair/SIGMA-MD' ;
 global.location = 'Lahore Pakistan' ;
 global.timezone  = process.env.TIME_ZONE || 'Asia/Karachi'
-global.gurl = 'https://youtube.com/c/SuhailTechInfo' ; // add your username
+global.gurl = 'https://youtube.com/c/' ; // add your username
 global.sudo =  process.env.SUDO || "27718060308" ;
 global.devs = "923466319114"; //Dont change it From here
 global.mztit = process.env.MZTIT ||"🅼♥︎❚❚♥︎🆉",
 global.Gname = process.env.GNAME ||"xxx",
 global.waUrl = process.env.WAURL ||"https://chat.whatsapp.com/Ckwh1CMUjXV1Vq5JCBycCK",
 global.website = 'https://github.com/SuhailTechInfo/Secktor-Md' ; //wa.me/+91000000000000
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://raw.githubusercontent.com/SecktorBot/Brandimages/main/logos/SocialLogo%201.png' ;
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/2ca599cf20ac50ba941f6.jpg' ;
 module.exports = {
   sessionName: process.env.SESSION_ID || '',      //Put Your Session Id Here
   botname: process.env.BOT_NAME || 'NO-MD',
-  ownername:  process.env.OWNER_NAME || `It'x Suhail`,
-  author:  process.env.PACK_AUTHER || '', 
+  ownername:  process.env.OWNER_NAME || `Maher Zubair`,
+  author:  process.env.PACK_AUTHER || 'Zubi', 
   auto_read_status : process.env.AUTO_READ_STATUS || 'true',
-  packname:  process.env.PACK_NAME || "\t   𓅋 ₊₉₂⃗⃗₃₁᩺₈ͦ₄ͪ₄ᷧ₇ͥ₄ᷞ₁⃗₇₆ 𓃮 \n\nᴋɪss ʍᴇ...💋\n\nᴄʟօsᴇ ყσυɾ ᴇყᴇs...👁️🙈\n\nʍɪss ʍᴇ...♥️\n\n\n   -ᴘʟᴇᴀsᴇ ᴅᴏɴ'ᴛ ᴛᴀӄᴇ 🚮 \n\n--- ρυвʟɪᴄ вσт нᴇʀᴇ ---" ,
-  autoreaction: process.env.AUTO_REACTION || 'on',
+  packname:  process.env.PACK_NAME || "SIGMA" ,
+  autoreaction: process.env.AUTO_REACTION || 'true',
   antibadword : process.env.ANTI_BAD_WORD || 'nobadwordokey',
-  alwaysonline: process.env.ALWAYS_ONLINE || 'false',
+  alwaysonline: process.env.ALWAYS_ONLINE || 'true',
   antifake :   process.env.FAKE_COUNTRY_CODE ||'212',
-  readmessage: process.env.READ_MESSAGE || 'false',
+  readmessage: process.env.READ_MESSAGE || 'true',
   HANDLERS: process.env.PREFIX || '.',
   warncount : process.env.WARN_COUNT || 2,
   disablepm: process.env.DISABLE_PM || "false",
-  MsgsInLog:process.env.MSGS_IN_LOG ||'true',
+  MsgsInLog:process.env.MSGS_IN_LOG ||'false',
   pmMsgsInLog:process.env.PM_MSGS_IN_LOGS ||'false',
-  levelupmessage: process.env.LEVEL_UP_MESSAGE || 'false',
+  levelupmessage: process.env.LEVEL_UP_MESSAGE || 'true',
   antilink: process.env.ANTILINK_VALUES || 'chat.whatsapp.com',
   antilinkaction: process.env.BRANCH || 'remove',
   BRANCH: process.env.BRANCH || 'Main',
@@ -47,7 +47,7 @@ module.exports = {
   HEROKU_API_KEY: process.env.HEROKU_API_KEY,
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "34wcCGPoe3yaGtpiBZgx4SN7",
   caption :process.env.CAPTION || "\t*•ᴘᴏᴡᴇʀᴇᴅ ʙʏ Maher Zubair•* ",   //*『sᴜʙsᴄʀɪʙᴇ • sᴜʜᴀɪʟ ᴛᴇᴄʜ』*\n youtube.com/@suhailtechinfo0"),
-  promote_demote_messages : process.env.PROMOTE_DEMOTE_MESSAGES || 'false' ,
+  promote_demote_messages : process.env.PROMOTE_DEMOTE_MESSAGES || 'true' ,
   OPENAI_API_KEY: process.env.OPENAI_API_KEY ||'' ,
   VERSION: process.env.VERSION || 'v.1.0.0',
   LANG: process.env.THEME|| 'SHELBY',
