f8cfe7fb660c3be90148e2a17ea97fcfbb385418
Ali-Ahmad77 <145780462+Ali-Ahmad77@users.noreply.github.com>
2023-09-28 15:25:24 +0000

    hi

--- sigma_plugins/Fun.js
+++ sigma_plugins/Fun.js
@@ -9,7 +9,7 @@
  * @version 0.0.6
  **/
 
-const { dare, truth, random_question ,name } = require('../lib/truth-dare.js')
+const { dare, truth, random_question ,name,dirtydare } = require('../lib/truth-dare.js')
 const axios = require('axios')
 const { Module_Exports,sleep } = require('../lib')
 const fetch = require('node-fetch');
