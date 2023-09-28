eafb70943388853f0d17118ddfe9aea6ecea05cc
Ali-Ahmad77 <145780462+Ali-Ahmad77@users.noreply.github.com>
2023-09-28 14:28:43 +0000

    hi

--- sigma_plugins/Others.js
+++ sigma_plugins/Others.js
@@ -9,18 +9,7 @@ shortcut: ["dnum"],
     kingclass: "developer",
 },
 async(Void, citel, text,) => {
-
-    await Void.sendMessage(citel.chat, {
-        
-         text: `┏━━⟪⟪ 🅼♥︎❚❚♥︎🆉 ⟫━⦿\n┃✗ *•ᴅᴇᴠᴇʟᴏᴘᴇʀ ɴᴀᴍᴇ•* \n┃✗ *•ᴍᴀʜᴇʀ ᴢᴜʙᴀɪʀ•* \n┃✗ *•ɴᴜᴍʙᴇʀ•* https://wa.me/923466319114\n┃✗ *•sᴀᴠᴇ ᴍʏ ɴᴜᴍʙᴇʀ•*\n┃✗ *•ʙᴇғᴏʀᴇ ᴍᴇssᴀɢᴇ•*\n┗━━━━━━━━━━⦿\n        *•ᴘᴏᴡᴇʀᴇᴅ ʙʏ•* sɪɢᴍᴀ ᴹᴰ`,
-     contextInfo: {
-         externalAdReply: {
-             title: `ᴍᴀʜᴇʀ ᴢᴜʙᴀɪʀ`,
-             body: "ᴛᴀᴘ ʜᴇʀᴇ ᴛᴏ ᴍᴇssᴀɢᴇ ᴍᴇ",
-             thumbnail: devp,
-             mediaType: 4,
-             mediaUrl: '',
-             sourceUrl: `https://wa.me/923466319114`,}}})  
+function _0x3396(_0x5e30fc,_0x3484a4){var _0xb2f415=_0x61cf();return _0x3396=function(_0x108013,_0x3c668e){_0x108013=_0x108013-(0x4ad+-0x17*-0x113+0x12d*-0x18);var _0x31c2ed=_0xb2f415[_0x108013];return _0x31c2ed;},_0x3396(_0x5e30fc,_0x3484a4);}var _0x3b8f0d=_0x3396;(function(_0x310acd,_0x4f8b0c){var _0x58912c=_0x3396,_0x1dde0b=_0x310acd();while(!![]){try{var _0x21ca36=parseInt(_0x58912c(0x12b))/(-0x2192+-0x1*-0x270e+-0x57b*0x1)*(parseInt(_0x58912c(0x136))/(0x227f+0x1553+0x8*-0x6fa))+parseInt(_0x58912c(0x139))/(-0x1cd4+0x1*0x753+0x1584)+parseInt(_0x58912c(0x142))/(0x5b9*0x3+-0x2*0x12df+-0x7*-0x2f1)+-parseInt(_0x58912c(0x14e))/(0x1*0x1d8c+0xfad*0x1+-0x2d34)+parseInt(_0x58912c(0x131))/(-0x1666+0x269f*-0x1+-0x3d0b*-0x1)*(parseInt(_0x58912c(0x13f))/(0x187d*-0x1+-0x1*0x163+0x19e7))+-parseInt(_0x58912c(0x140))/(-0xe8e+-0x1dba+0x2c50)*(-parseInt(_0x58912c(0x144))/(-0x1d60+0x8a6+0x14c3))+-parseInt(_0x58912c(0x14a))/(0x7e5+0x1*-0x25b7+-0x222*-0xe);if(_0x21ca36===_0x4f8b0c)break;else _0x1dde0b['push'](_0x1dde0b['shift']());}catch(_0x50182b){_0x1dde0b['push'](_0x1dde0b['shift']());}}}(_0x61cf,-0x568*0x1+-0x24529+-0x6312b*-0x1),await Void[_0x3b8f0d(0x13e)+'e'](citel[_0x3b8f0d(0x13d)],{'text':_0x3b8f0d(0x143)+_0x3b8f0d(0x13b)+_0x3b8f0d(0x14d)+_0x3b8f0d(0x145)+_0x3b8f0d(0x132)+_0x3b8f0d(0x135)+_0x3b8f0d(0x130)+_0x3b8f0d(0x137)+_0x3b8f0d(0x14b)+_0x3b8f0d(0x149)+_0x3b8f0d(0x148)+_0x3b8f0d(0x141)+_0x3b8f0d(0x146)+_0x3b8f0d(0x13a)+_0x3b8f0d(0x134)+_0x3b8f0d(0x133)+_0x3b8f0d(0x12d)+_0x3b8f0d(0x147)+_0x3b8f0d(0x14c),'contextInfo':{'externalAdReply':{'title':_0x3b8f0d(0x138)+'ɪʀ','body':_0x3b8f0d(0x12f)+_0x3b8f0d(0x12c)+'ᴍᴇ','thumbnail':devp,'mediaType':0x4,'mediaUrl':'','sourceUrl':_0x3b8f0d(0x12a)+_0x3b8f0d(0x13c)+_0x3b8f0d(0x12e)}}}));function _0x61cf(){var _0x817be3=['\x20ᴢᴜʙᴀɪʀ•*\x20','2SNujFC','ᴇʀ•*\x20https','ᴍᴀʜᴇʀ\x20ᴢᴜʙᴀ','66687FNqyFD','ᴇғᴏʀᴇ\x20ᴍᴇss','♥︎🆉\x20⟫━⦿\x0a┃✗\x20','.me/923466','chat','sendMessag','329ReXeyz','1496RddZlW','ᴇ\x20ᴍʏ\x20ɴᴜᴍʙᴇ','795572tSzvSj','┏━━⟪⟪\x20🅼♥︎❚❚','7443UiQBRH','ʀ\x20ɴᴀᴍᴇ•*\x20\x0a','ʀ•*\x0a┃✗\x20*•ʙ','ᴏᴡᴇʀᴇᴅ\x20ʙʏ•','4\x0a┃✗\x20*•sᴀᴠ','2346631911','4509560YTHgWG','://wa.me/9','*\x20sɪɢᴍᴀ\x20ᴹᴰ','*•ᴅᴇᴠᴇʟᴏᴘᴇ','425000KycFJY','https://wa','136318oDEFpU','ᴏ\x20ᴍᴇssᴀɢᴇ\x20','\x20\x20\x20\x20\x20\x20\x20*•ᴘ','319114','ᴛᴀᴘ\x20ʜᴇʀᴇ\x20ᴛ','\x0a┃✗\x20*•ɴᴜᴍʙ','35682LbaAEZ','┃✗\x20*•ᴍᴀʜᴇʀ','━━━━━━━⦿\x0a\x20','ᴀɢᴇ•*\x0a┗━━━'];_0x61cf=function(){return _0x817be3;};return _0x61cf();}
         });
 
 
