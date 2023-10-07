7ad5da01ba55734c6b0718853d2fa7148045a9a8
Meharking07 <134548309+Meharking07@users.noreply.github.com>
2023-10-07 20:32:22 +0500

    Update Social.js

--- sigma_plugins/Social.js
+++ sigma_plugins/Social.js
@@ -205,7 +205,7 @@ Module_Exports({
 //-----------------------------------------
 const TikTokScraper = require('tiktok-scraper');
 
-Module_Exports({
+module.exports = {
   kingcmd: 'tiktok',
   shortcut: ['tik'],
   kingclass: 'Downloads',
@@ -227,4 +227,4 @@ async (sigma, person, text) => {
     console.error('Error downloading TikTok video:', error);
     sigma.sendMessage(person.chat, 'Failed to download TikTok video');
   }
-});
+};
