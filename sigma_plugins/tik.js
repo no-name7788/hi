035a3764aeda29e94fe772a167004876fb75b3d9
Meharking07 <134548309+Meharking07@users.noreply.github.com>
2023-10-07 21:42:25 +0500

    Create tik.js

---
+++ sigma_plugins/tik.js
@@ -0,0 +1,28 @@
+
+const {Module_Exports , name , prefix,getBuffer,tlang,fetchJson } = require('../lib')
+const fetch = require('node-fetch');
+const bocil = require('@bochilteam/scraper');
+const cheerio = require('cheerio')
+const axios= require('axios');
+const ttdl = require('btch-downloader');
+
+
+const { tiktokdl } = require('tiktokdl');
+
+Module_Exports({
+  kingcmd: 'tiktok',
+  shortcut: ['tik'],
+  kingclass: 'Downloads',
+  infocmd: 'Download videos from TikTok.'
+},
+
+async (sigma, person, text) => {
+const url = text.split(' ')[1];
+
+  try {
+   const data = await tiktokdl(url);
+   sigma.sendMessage(person.chat,{text: 'TikTok video downloaded successfully!'});
+  } catch (error) {
+   sigma.sendMessage(person.chat,{text: 'Error downloading TikTok video: ' + error.message});
+   }
+   });
