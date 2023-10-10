7e0a668b3a0b2e088d5b7e886e3697f7d1585467
Meharking07 <134548309+Meharking07@users.noreply.github.com>
2023-10-10 03:13:42 +0000

    hi

--- sigma_plugins/tik.js
+++ sigma_plugins/tik.js
@@ -11,18 +11,19 @@ const ttdl = require('btch-downloader');
 const { tiktokdl } = require('tiktokdl');
 
 Module_Exports({
-  kingcmd: 'tiktok',
-  shortcut: ['tk'],
-  kingclass: 'Downloads',
-  infocmd: 'Download videos from TikTok.'
+  kingcmd: "tiktok",
+shortcut :  ['tk','ttdl'],
+  infocmd: "Downloads Tiktok Videos Via Url.",
+  kingclass: "downloader",
+  use: "paste tiktok video link",
 },
- async (sigma, person, text) => {
-    const url = text.split(' ')[1];
 
-    
-      const response = await tiktokdl(url);
-      sigma.sendMessage(person.chat, {video : {url : video } ,caption: `*╰┈➤ 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 ${name.botname}*`,height: 470,width: 540,  } , {quoted : person });
-    
-    
+async(sigma, person, memo) => {
+  if(!memo) return await person.reply(`*_Give me tiktok video link_*`);
+  let txt = memo ? memo.split(" ")[0]:'';
+  if (!/tiktok/.test(txt)) return await person.reply(`*_Please give me valid tiktok video link..!_*`);
+  const { status ,thumbnail, video, audio } = await tiktokdl(txt)
+  //console.log("url : " , video  ,"\nThumbnail : " , thumbnail ,"\n Audio url : " , audio )
+  if (status) return await sigma.sendMessage(person.chat, {video : {url : video } ,caption: `*╰┈➤ 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 ${name.botname}*`,height: 470,width: 540,  } , {quoted : person });
+  else return await person.reply("Error while downloading your video") 
 })
-
