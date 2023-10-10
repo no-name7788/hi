3c7ed54b68754149e72458227352942e1b0449b9
Meharking07 <134548309+Meharking07@users.noreply.github.com>
2023-10-10 03:32:54 +0000

    hi

--- sigma_plugins/tik.js
+++ sigma_plugins/tik.js
@@ -8,7 +8,7 @@ const ttdl = require('btch-downloader');
 
 
 
-const { tiktokdl } = require('tiktokdl');
+const { tikd } = require('tiktokdl');
 
 Module_Exports({
   kingcmd: "tiktok",
@@ -22,7 +22,7 @@ async(sigma, person, memo) => {
   if(!memo) return await person.reply(`*_Give me tiktok video link_*`);
   let txt = memo ? memo.split(" ")[0]:'';
   if (!/tiktok/.test(txt)) return await person.reply(`*_Please give me valid tiktok video link..!_*`);
-  const { status ,thumbnail, video, audio } = await tiktokdl(txt)
+  const { status ,thumbnail, video, audio } = await tikd(txt)
   //console.log("url : " , video  ,"\nThumbnail : " , thumbnail ,"\n Audio url : " , audio )
   if (status) return await sigma.sendMessage(person.chat, {video : {url : video } ,caption: `*╰┈➤ 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 ${name.botname}*`,height: 470,width: 540,  } , {quoted : person });
   else return await person.reply("Error while downloading your video") 
