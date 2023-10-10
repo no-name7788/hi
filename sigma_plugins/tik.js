a5dad5fab11ba89eeb8e95b220f01a4af3ea67b7
Meharking07 <134548309+Meharking07@users.noreply.github.com>
2023-10-10 03:41:52 +0000

    hi

--- sigma_plugins/tik.js
+++ sigma_plugins/tik.js
@@ -20,7 +20,9 @@ Module_Exports({kingcmd: "facebook",
         if (!memo) return person.reply(`Give me facebook video link`);
          let txt = memo ? memo.split(" ")[0]:'';
  if (!/tiktok/.test(txt)) return await person.reply(`Please give me valid facebook video link..!`);
-         ttdl.tiktoklv2(memo).then(async (data) =>
-          {  return sigma.sendMessage(person.chat, { video: { url: data.result[0].url },caption: `╰┈➤ 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 ${name.botname}`,width: 550,height: 470, },{ quoted: person }) }) 
+ const url = 'https://www.tiktok.com/@omagadsus/video/7025456384175017243?is_from_webapp=1&sender_device=pc&web_id6982004129280116226'
+ // Using tiktokdl
+ const data = await ttdl(url)       
+          {  return sigma.sendMessage(person.chat, { video: { url: data.result[0].url },caption: `╰┈➤ 𝙶𝙴𝙽𝙴𝚁𝙰𝚃𝙴𝙳 𝙱𝚈 ${name.botname}`,width: 550,height: 470, },{ quoted: person }) }
          return person.reply("Error while downloading your video") 
 })
