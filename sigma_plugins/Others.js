599145692bb9c1e7db3a75e6e7d467961362656d
Meharking07 <134548309+Meharking07@users.noreply.github.com>
2023-10-21 02:34:42 +0500

    Update Others.js

--- sigma_plugins/Others.js
+++ sigma_plugins/Others.js
@@ -44,7 +44,7 @@ try{
 var sigma_time = new Date().toLocaleString('HI', { timeZone: global.timezone }).split(' ')[1]
 var sigma_date = new Date().toLocaleDateString(get_localized_date)
 var sigma_wish = ''
-if (hrs < 12) sigma_wish = 'ɢᴏᴏᴅ ᴍᴏʀɴɪɴɢ ⛅'
+if (hrs >= 1 && hrs <= 11 sigma_wish = 'ɢᴏᴏᴅ ᴍᴏʀɴɪɴɢ ⛅'
 if (hrs >= 11 && hrs <= 17) sigma_wish = 'ʙᴇᴅ ᴛɪᴍᴇ 🌙'
 if (hrs >= 17 && hrs <= 24) sigma_wish = 'ɢᴏᴏᴅ ᴀғᴛᴇʀɴᴏᴏɴ 🌞'
 //if (hrs >= 20 && hrs <= 24) sigma_wish = 'ʙᴇᴅ ᴛɪᴍᴇ 🌙'
